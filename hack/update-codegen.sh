9bd84627adaac915555e5b62b1a7998937d469c8
baowj <bwj_678@qq.com>
2024-03-11 20:33:56 +0800

    Add rulecheck CRD 1.add rulecheck CRD 2.generate rulecheck deepcopy, list, informer, clientset 3.add hack files
    Signed-off-by: baowj <bwj_678@qq.com>

---
+++ hack/update-codegen.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021 THL A29 Limited, a Tencent company.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+#
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
+CODEGEN_PKG=$(go env GOPATH)/pkg/mod/k8s.io/code-generator@v0.24.2-rc.0
+echo $SCRIPT_ROOT
+# generate the code with:
+# --output-base    because this script should also be able to run inside the vendor dir of
+#                  k8s.io/kubernetes. The output-base is needed for the generators to output into the vendor dir
+#                  instead of the $GOPATH directly. For normal projects this can be dropped.
+bash "${CODEGEN_PKG}"/generate-groups.sh "deepcopy,client,informer,lister" \
+  github.com/tencent/caelus/pkg/generated github.com/tencent/caelus/pkg/apis \
+  caelus:v1 \
+  --output-base "$(dirname "${BASH_SOURCE[0]}")/../../../.." \
+  --go-header-file "${SCRIPT_ROOT}"/hack/boilerplate.go.txt
+
+# To use your own boilerplate text append:
+#   --go-header-file "${SCRIPT_ROOT}"/hack/custom-boilerplate.go.txt
---
+++ pkg/apis/caelus/v1/rulecheck_types.go
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2021 THL A29 Limited, a Tencent company.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1
+
+import (
+	"encoding/json"
+	"github.com/tencent/caelus/pkg/util/times"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// RuleCheck is a specification for a RuleCheck resource
+type RuleCheck struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   RuleCheckSpec   `json:"spec"`
+	Status RuleCheckStatus `json:"status"`
+}
+
+// RuleCheckSpec is the spec for a RuleCheck resource
+type RuleCheckSpec struct {
+	NodeSelector map[string]string `json:"nodeSelector"`
+
+	Metrics []string `json:"metrics"`
+	// CheckInterval describes the interval to trigger detection
+	CheckInterval *times.Duration `json:"checkInterval"`
+	// HandleInterval describes the interval to handle conflicts after detecting abnormal result
+	HandleInterval *times.Duration `json:"handleInterval"`
+	// RecoverInterval describes the interval to recover conflicts after detecting normal result
+	RecoverInterval *times.Duration `json:"recoverInterval"`
+
+	Rules        []*DetectActionRules `json:"rules"`
+	RecoverRules []*DetectActionRules `json:"recoverRules"`
+}
+
+// DetectActionRules define detectors and actions
+type DetectActionRules struct {
+	Detects []*DetectAction `json:"detects"`
+	Actions []*DetectAction `json:"actions"`
+}
+
+// DetectAction define detector config
+type DetectAction struct {
+	Name string          `json:"name"`
+	Args json.RawMessage `json:"args"`
+}
+
+// ActionConfig define action config
+type ActionConfig struct {
+	Name string          `json:"name"`
+	Args json.RawMessage `json:"args"`
+}
+
+// RuleCheckStatus is the status for a RuleCheck resource
+type RuleCheckStatus struct {
+	AvailableReplicas int32 `json:"availableReplicas"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// RuleCheckList is a list of RuleCheck resources
+type RuleCheckList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata"`
+
+	Items []RuleCheck `json:"items"`
+}
